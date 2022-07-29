5031537adc7c2f5dd5990248250c0f427451009a
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-28 20:19:39 -0500

    updated models rename admin schema to users

--- backend/API/api.v1.js
+++ backend/API/api.v1.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const studentRouter = require("../routes/student.routes")
+const studentsRouter = require("../routes/student.routes")
+const postsRouter = require("../routes/post.routes")
+const eventsRouter = require("../routes/event.routes")
+const adminsRouter = require("../routes/admin.routes")
 
-router.use("/students/", studentRouter);
+router.use("/students/", studentsRouter);
+router.use("/posts/", postsRouter);
+router.use("/events/", eventsRouter);
+router.use("/admin/", adminsRouter);
 
 
 
--- backend/models/event.schema.js
+++ backend/models/event.schema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const eventsSchema = mongoose.Schema({
     title:{type:String, required:true},
     description:String,
-    start:{type:Date, default:Date.now},
-    end:{type:Date, default:Date.now}
+    start:{type:Date, default:()=> Date.now()},
+    end:{type:Date, default:()=> Date.now()}
 
 });
-module.exports = mongoose.model("CalandarEvent", eventsSchema);
+module.exports = mongoose.model("Event", eventsSchema);
