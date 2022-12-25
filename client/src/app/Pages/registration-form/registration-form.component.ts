f272b88e2fd7030356711694277044cf24c2afa5
Aldaine Clarke <vainedev@gmail.com>
2022-12-25 18:29:22 -0500

    separated registration form fields into different form group

--- client/src/app/Pages/registration-form/registration-form.component.ts
+++ client/src/app/Pages/registration-form/registration-form.component.ts
@@ -12,29 +12,41 @@ export class RegistrationFormComponent implements OnInit {
   parishes = ["Hanover", "St. James", "Trelawny", "St. Ann", "St. Mary", "Portland", "Westmoreland", "St. Elizabeth", "Manchester", "Clarendon", "St. Catherine", "Kingston", "St. Andrew", "St. Thomas"];
 
   hasAgreed =false;
+  // TODO
+  // Break Form into separate form groups to allow to stepper to better navigate different sections of the form.
 
   registrationForm = new FormGroup({
-    fname: new FormControl(""),
-    lname: new FormControl(""),
-    mname: new FormControl(""),
-    dob: new FormControl(""),
-    gender: new FormControl(""),
-    parent_name: new FormControl(""),
-    parent_email:new FormControl(""),
-    parent_address: new FormControl(""),
-    parent_tel:new FormControl(""),
-    physician_name:new FormControl(""),
-    physician_address:new FormControl(""),
-    physician_office:new FormControl(""),
-    physician_tel:new FormControl(""),
-    alergies: new FormControl(),
-    contacts:new FormGroup({
-        fullname: new FormControl(""),
-        relationship: new FormControl(""),
-        address:new FormControl(""),
-  }), 
+
+    childInfo: new FormGroup({
+      fname: new FormControl(""),
+      lname: new FormControl(""),
+      mname: new FormControl(""),
+      dob: new FormControl(""),
+      gender: new FormControl(""),
+    }),
+    guardianInfo: new FormGroup({
+      parent_name: new FormControl(""),
+      parent_email:new FormControl(""),
+      parent_addr_1: new FormControl(""),
+      parent_addr_2: new FormControl(""),
+      parent_addr_city: new FormControl(""),
+      parent_addr_parish: new FormControl(""),
+      parent_tel:new FormControl(""),
+    }),
+
+    healthInfo: new FormGroup({
+      physician_name:new FormControl(""),
+      physician_email:new FormControl(""),
+      physician_address:new FormControl(""),
+      physician_office:new FormControl(""),
+      physician_tel:new FormControl(""),
+      alergies: new FormControl(""),
+    }),
     
   });
+  showData(){
+    console.log(this.registrationForm);
+  }
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
